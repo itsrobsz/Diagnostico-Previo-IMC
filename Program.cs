6acb5d61dbffb1474e133da2b7a9b23489d0ea5a
Roberta Oliveira <102633229+itsrobsz@users.noreply.github.com>
2022-06-17 11:19:17 -0300

    apresentacao do programa
    apresentação e validações iniciais do programa

--- Program.cs
+++ Program.cs
@@ -6,6 +6,25 @@ namespace Desafio_IMC
     {
         static void Main(string[] args)
         {
+            // Apresentação do Programa
+            Console.WriteLine("Bem vindo(a) ao programa de Diagnóstico Prévio!");
+            Console.WriteLine("Precisaremos de alguns dados para podermos realizar o cálculo, tudo bem? (SIM OU NÃO)");
+            string aceito = Console.ReadLine();
+
+            //Validação dos dados digitados pelo usuário
+            while (aceito.ToLower() != "sim")
+            {
+                Console.WriteLine("Precisamos de uma confirmação válida. Por favor, digite SIM para que possamos continuar.");
+                aceito = Console.ReadLine();
+
+                if (aceito.ToLower() == "não")
+                {
+                    Console.WriteLine("Entendemos que você NÃO queira prosseguir. Até uma próxima e siga se cuidando!");
+                    break;
+                }
+            }
+            
+
 
         }
     }
