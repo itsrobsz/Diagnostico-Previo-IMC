4a740d4cf84ae9ee41ca9e2ce1ce329ae964a2dd
Roberta Oliveira <102633229+itsrobsz@users.noreply.github.com>
2022-06-19 22:25:27 -0300

    correcoes
    correções e adições de validações e adição de comentários

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ namespace Desafio_IMC
             Console.WriteLine("Precisaremos de alguns dados para podermos realizar o cálculo, tudo bem? (SIM OU NÃO)");
             string aceito = Console.ReadLine();
 
-            //Validação dos dados digitados pelo usuário
-            while(aceito.ToLower() != "sim" && aceito.ToLower() != "não")
+            // Validação dos dados digitados pelo usuário 
+            while (aceito.ToLower() != "sim" && aceito.ToLower() != "não")
             {
                 Console.WriteLine("Precisamos de uma confirmação válida. Por favor, digite SIM para que possamos continuar ou NÃO caso queira sair.");
                 aceito = Console.ReadLine();
@@ -31,7 +31,7 @@ namespace Desafio_IMC
                 Environment.Exit(0);
             }
 
-            //Declaração das variáveis
+            // Declaração das variáveis
             string nome;
             string sexo;
             int idade;
@@ -39,17 +39,19 @@ namespace Desafio_IMC
             double peso;
             double imc;
 
-            
-            Console.WriteLine("Agora, digite seu nome: (Exemplo: José da Silva)");
+
+            // Solicitação do preenchimento da variável nome pelo usuário
+            Console.WriteLine("Agora, digite seu nome: \n(Exemplo: José da Silva)");
             nome = Console.ReadLine();
 
 
+            // Solicitação do preenchimento da variável sexo pelo usuário
             Console.WriteLine("Digite seu sexo: (M ou F)");
             sexo = Console.ReadLine();
 
             while (sexo.ToUpper() != "M" && sexo.ToUpper() != "F")
             {
-                Console.WriteLine("Sexo não encontrado! Por favor, insira o dado conforme o exemplo.");
+                Console.WriteLine("Sexo não encontrado! Por favor, insira o dado conforme especificado entre parênteses.");
                 sexo = Console.ReadLine();
             }
 
@@ -63,8 +65,21 @@ namespace Desafio_IMC
                 sexo = "Feminino";
             }
 
+
+            // Solicitação do preenchimento da variável idade pelo usuário
             Console.WriteLine("Nos informe sua idade: ");
-            idade = int.Parse(Console.ReadLine());
+            var idade1 = (Console.ReadLine());
+
+            var seila = int.TryParse(idade1, out idade);
+
+            // Validações e definição de limites para idade
+            while (seila == false)
+            {
+                Console.WriteLine("Por favor, digite uma idade. Se tiver tentando inserir letras, coloque números.");
+                Console.WriteLine("Nos informe sua idade: ");
+                idade1 = (Console.ReadLine());
+                seila = int.TryParse(idade1, out idade);
+            }
 
             while (idade < 0)
             {
@@ -78,13 +93,35 @@ namespace Desafio_IMC
                 idade = int.Parse(Console.ReadLine());
             }
 
-            Console.WriteLine("Insira sua altura, (em metros): ");
-            Console.WriteLine("Utilize vírgula, não ponto. ");
-            altura = double.Parse(Console.ReadLine());
-            
-            while(altura < 0)
+
+            // Solicitação do preenchimento da variável altura pelo usuário
+            Console.WriteLine("Insira sua altura (em metros): ");
+            Console.WriteLine("Um lembrete: Utilize vírgula, não ponto. ");
+            var altura1 = (Console.ReadLine());
+
+
+            // Uso do TryParse para evitar erro de exceções
+            var seila2 = double.TryParse(altura1, out altura);
+
+            while (seila2 == false)
             {
-                Console.WriteLine("Da mesma forma que não existe idade negativa, não existe altura negativa!");
+                Console.WriteLine("Por favor, digite uma altura para prosseguir.\n Digite números, não letras.");
+                Console.WriteLine("(Lembre-se: em metros e separada por vírgula.)");
+                altura1 = (Console.ReadLine());
+                seila2 = double.TryParse(altura1, out altura);
+            }
+
+            // Validação e limites de altura
+            while (altura == 0)
+            {
+                Console.WriteLine("Você deve medir alguma coisa, no mínimo!");
+                Console.WriteLine("Verifique o que foi digitado e tente novamente.");
+                altura = double.Parse(Console.ReadLine());
+            }
+
+            while (altura < 0)
+            {
+                Console.WriteLine("Da mesma forma que não existe idade negativa, não existe altura negativa.");
                 Console.WriteLine("Tente novamente! (Exemplo: 1,78)");
                 altura = double.Parse(Console.ReadLine());
             }
@@ -92,24 +129,46 @@ namespace Desafio_IMC
             while (altura > 2.80)
             {
                 Console.WriteLine("A pessoa mais alta a existir na história possui 2,51 metros de altura.");
-                Console.WriteLine("Por favor, insira uma altura realista e separada por vírgula.");
+                Console.WriteLine("Por favor, insira uma altura dentro dessa média. Se já for o caso, separá-la por vírgula.");
                 altura = double.Parse(Console.ReadLine());
             }
 
+            
+            // Solicitação do preenchimento da variável peso pelo usuário
             Console.WriteLine("Digite seu peso, em kg: ");
-            peso = double.Parse(Console.ReadLine());
+            var peso1 = (Console.ReadLine());
 
+
+            // Utilização do TryParse para evitar erros de exceções
+            var seila3 = double.TryParse(peso1, out peso);
+
+
+            while (seila3 == false)
+            {
+                Console.WriteLine("Impossível captar peso em letras.");
+                Console.WriteLine("Por favor, digite seu peso. Lembre-se de separá-lo por vírgula e não ponto!");
+                peso1 = (Console.ReadLine());
+                seila3 = double.TryParse(peso1, out peso);
+            }
+
+
+            // Validações e definições de limites para peso
+            while (peso == 0)
+            {
+                Console.WriteLine("Todos já nascemos com algum peso. Verifique o que foi digitado e tente novamente!");
+                peso = double.Parse(Console.ReadLine());
+            }
 
             while (peso < 0)
             {
-                Console.WriteLine("Novamente na casa dos negativos... Não podemos prosseguir!");
+                Console.WriteLine("Na casa dos negativos... Não podemos prosseguir!");
                 Console.WriteLine("Digite novamente um peso (que seja positivo!): ");
                 peso = double.Parse(Console.ReadLine());
             }
 
             while (peso > 595)
             {
-                Console.WriteLine("A pessoa mais pesada a viver no mundo tinha 595kg. Será mesmo que você pesa tudo isso?");
+                Console.WriteLine("A pessoa mais pesada a viver no mundo tinha 595kg. Digite um peso dentro desse parâmetro.");
                 Console.WriteLine("Digite um peso: ");
                 peso = double.Parse(Console.ReadLine());
             }
@@ -117,6 +176,12 @@ namespace Desafio_IMC
             Console.ReadLine();
 
 
+            /// <summary>
+            /// Função que verifica a categoria a qual uma pessoa pertence de acordo com a sua idade
+            /// Recebe um argumento do tipo inteiro
+            /// </summary>
+            /// <param name="x"></param>
+            /// <returns>Retorna uma string após a verificação</returns>
             static string VerificarCategoria(int x)
             {
                 if (x < 12)
@@ -139,10 +204,17 @@ namespace Desafio_IMC
                     return "Idoso";
                 }
             }
-            //Cálculo do imc para futuras verificações em funções
+
+            // Cálculo do imc para futuras verificações em funções
             imc = peso / (altura * altura);
 
-            
+
+            /// <summary>
+            /// Função que verifica os riscos a qual uma pessoa pertence de acordo com o IMC calculado
+            /// Recebe um argumento do tipo double
+            /// </summary>
+            /// <param name="y"></param>
+            /// <returns>Retorna uma string após a verificação</returns>
             static string VerificarRiscos(double y)
             {
                 if (y < 20)
@@ -150,28 +222,34 @@ namespace Desafio_IMC
                     return "Muitas complicações de saúde como doenças pulmonares e cardiovasculares podem estar associadas ao baixo peso.";
                 }
 
-                else if(y >= 20 && y <= 24)
+                else if (y >= 20 && y <= 24)
                 {
                     return "Seu peso está ideal para suas referências.";
                 }
 
-                else if(y >= 25 && y <= 29)
+                else if (y >= 25 && y <= 29)
                 {
                     return "Aumento de peso apresenta risco moderado para outras doenças crônicas e cardiovasculares.";
                 }
 
-                else if (y >=30 && y <= 35)
+                else if (y >= 30 && y <= 35)
                 {
                     return "Quem tem obesidade vai estar mais exposto a doenças graves e ao risco de mortalidade.";
                 }
 
                 else
                 {
-                    return "O obeso mórbido vive menos, tem alto risco de mortalidade geral por diversas causas";
+                    return "O obeso mórbido vive menos, tem alto risco de mortalidade geral por diversas causas.";
                 }
             }
 
-            
+
+            /// <summary>
+            /// Função que verifica as recomendações iniciais que uma pessoa deve seguir, de acordo com o IMC calculado
+            /// Recebe um argumento do tipo double
+            /// </summary>
+            /// <param name="z"></param>
+            /// <returns>Retorna uma string após a verificação</returns>
             static string VerificarRecomendacoes(double z)
             {
                 if (z < 20)
@@ -209,22 +287,14 @@ namespace Desafio_IMC
             Console.WriteLine("Altura: " + altura);
             Console.WriteLine("Peso: " + peso);
             Console.WriteLine("Categoria: " + VerificarCategoria(idade));
+            Console.WriteLine();
             Console.WriteLine("IMC Desejável: Entre 20 e 24");
-            Console.WriteLine("Resultado IMC: " + Math.Round(imc,2));
+            Console.WriteLine();
+            Console.WriteLine("Resultado IMC: " + Math.Round(imc, 2));
+            Console.WriteLine();
             Console.WriteLine("Riscos: " + VerificarRiscos(imc));
+            Console.WriteLine();
             Console.WriteLine("Recomendação Inicial: " + VerificarRecomendacoes(imc));
-
-
-
-
-
-
-
-
-
-
-
-
         }
     }
 }
